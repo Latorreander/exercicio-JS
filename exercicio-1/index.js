f0722b231c897b29ed3fd202ca85e07b828ed495
Latorreander <latorreander@gmail.com>
2022-05-12 11:47:45 -0300

    first commit

---
+++ exercicio-1/index.js
@@ -0,0 +1,4 @@
+//EXERCICIO JS BÁSICO:
+
+const nomeUsuario = prompt('Digite seu nome:')
+alert(`Seja Bem vindo(a), ${nomeUsuario}!`)
---
+++ exercicio-2/index.js
@@ -0,0 +1,9 @@
+const boletoPago = false
+
+if(boletoPago){
+    alert(`O Boleto está Pago!`)
+}else if(boletoPago === false){
+    alert(`O Boleto não está pago!`)
+}
+
+
