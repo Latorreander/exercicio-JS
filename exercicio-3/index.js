c899697e27fdf674df5915608c0fbac54809e3c4
Latorreander <latorreander@gmail.com>
2022-05-13 10:08:26 -0300

    exercicio de JS

---
+++ exercicio-3/index.js
@@ -0,0 +1,18 @@
+const perguntaHospedar = confirm('Seja Bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?')
+
+if(perguntaHospedar){
+    alert('Ótimo! Nós temos as melhores camas de toda a região!')
+}else{
+    alert('Que pena! Você parecia ser uma pessoa legal')
+}
+ 
+
+  
+
+
+
+
+
+
+
+
